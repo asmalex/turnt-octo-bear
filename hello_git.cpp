f019c284aed804ca0652827dba763cfc33080b59
Alex Reno <asmalex@gmail.com>
2014-09-03 18:11:22 -0700

    this is my second commit

--- hello_git.cpp
+++ hello_git.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 int main() {
-std::cout << "Hello git! I'mma learn you!";
+std::cout << "Hello git! I'mma learn you!" << std::endl;
+std::cout << "I've already made my first commit!" << std::endl;
 return 0;
 }
